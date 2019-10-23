91df3ec4c718adb3d523328f38e688152de153e5
LJZSR <43992884+LJZSR@users.noreply.github.com>
2019-10-23 17:37:41 +0800

    Create regression.py

---
+++ regression.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def loadDataSet(filename):
+    numFeat = len(open(filename).readline().split('\t')) - 1
+    dataMat = []
+    labelMat = []
+    fr = open(filename)
+    for line in fr.readlines():
+        lineArr = []
+        curLine = line.strip().split('\t')
+        for i in range(numFeat):
+            lineArr.append(float(curLine[i]))
+        dataMat.append(lineArr)
+        labelMat.append(float(curLine[-1]))
+    return dataMat, labelMat
+
+def standRegress(xArr, yArr):
+    xMat = np.mat(xArr)
+    yMat = np.mat(yArr).T
+    xTx = xMat.T * xMat
+    if np.linalg.det(xTx) == 0.0:
+        print("This matrix is singular, cannot do inverse")
+        return
+    ws = xTx.I * (xMat.T * yMat)
+    return ws
+
+xArr, yArr = loadDataSet("regression/ex0.txt")
+ws = standRegress(xArr, yArr)
+print(ws)
+xMat = np.mat(xArr)
+yMat = np.mat(yArr)
+yHat = xMat * ws
+
+print(np.corrcoef(yHat.T, yMat))
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(xMat[:, 1].flatten().A[0], yMat.T[:, 0].flatten().A[0])
+xCopy = xMat.copy()
+ax.plot(xCopy[:, 1], yHat)
+plt.show()
