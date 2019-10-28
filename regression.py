2eae4a11b3dd4f66fb58f13333df1e8ba1408382
LJZSR <43992884+LJZSR@users.noreply.github.com>
2019-10-28 22:13:26 +0800

    Update regression.py

--- regression.py
+++ regression.py
@@ -26,18 +26,37 @@ def standRegress(xArr, yArr):
     ws = xTx.I * (xMat.T * yMat)
     return ws
 
+
+def lwlr(testPoint, xArr, yArr, k = 1.0):
+    xMat = np.mat(xArr)
+    yMat = np.mat(yArr).T
+    m = np.shape(xArr)[0]
+    weights = np.mat(np.eye((m)))
+    for i in range(m):
+        diffMat = testPoint - xMat[i, :]
+        weights[i, i] = np.exp(diffMat * diffMat.T / (-2 * k ** 2))
+        xTx = xMat.T * weights * xMat
+        if np.linalg.det(xTx) == 0.0:
+            print("This matrix is singular, cannot do inverse")
+            return
+        ws = xTx.I * (xMat.T * weights * yMat)
+    return testPoint * ws
+
+def lwlrTest(testArr, xArr, yArr, k = 1.0):
+    m = np.shape(testArr)[0]
+    yHat = np.zeros(m)
+    for i in range(m):
+        yHat[i] = lwlr(testArr[i], xArr, yArr, k)
+    return yHat
+
+
 xArr, yArr = loadDataSet("regression/ex0.txt")
-ws = standRegress(xArr, yArr)
-print(ws)
 xMat = np.mat(xArr)
-yMat = np.mat(yArr)
-yHat = xMat * ws
-
-print(np.corrcoef(yHat.T, yMat))
-
+yHat = lwlrTest(xArr, xArr, yArr, 0.003)
+srtInd = xMat[:, 1].argsort(0)
+xSort = xMat[srtInd][:, 0, :]
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.scatter(xMat[:, 1].flatten().A[0], yMat.T[:, 0].flatten().A[0])
-xCopy = xMat.copy()
-ax.plot(xCopy[:, 1], yHat)
+ax.plot(xSort[:, 1], yHat[srtInd])
+ax.scatter(xMat[:, 1].flatten().A[0], np.mat(yArr).T.flatten().A[0], s=2, c='red')
 plt.show()
